ae9360ada04682bff1ecc9810efbe63d303ac9e6
MangoExtremis <144482551+MangoExtremis@users.noreply.github.com>
2024-05-23 20:20:19 -0700

    Added security.

---
+++ cdn/provider.js
@@ -0,0 +1,21 @@
+// **************
+// This is basic and just provides client-side security and is also enforced on the server.
+// This just exists because yes!
+// **************
+const blockedIPs = ['1.2.3.4'];
+
+fetch('https://api.ipify.org?format=json')
+.then(response => response.json())
+.then(data => {
+    const clientIP = data.ip;
+    if (blockedIPs.includes(clientIP)) {
+        window.location.replace("http://portunus.run.place/403.html");
+        document.body.innerHTML = '';
+    } else {
+        window.location.replace("http://portunus.run.place/403.html");
+        document.body.innerHTML = '';
+    }
+})
+.catch(error => {
+    console.error('[SECURITY ALERT] Failed to run security check:', error);
+});
