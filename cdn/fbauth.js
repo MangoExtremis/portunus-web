d302bf9803703a09464160cbbe98a082b3de886f
MangoExtremis <144482551+MangoExtremis@users.noreply.github.com>
2024-05-26 22:37:25 -0700

    Added fix for forgotpassword.

--- cdn/fbauth.js
+++ cdn/fbauth.js
@@ -240,7 +240,7 @@ if (forgotpasswordButton) {
                 document.getElementById("email").disabled = false;
                 document.getElementById("agreeCheckbox").disabled = false;
                 submitBtn.disabled = false;
-                submitBtn.innerHTML = "Login";
+                submitBtn.innerHTML = "Send Email";
             });
         } else {
             document.getElementById("message").innerHTML = "<p class='error-message'>You must confirm you are not a robot.</p>";
